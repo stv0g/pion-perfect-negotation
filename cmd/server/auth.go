bda8a46b5204d5507bcce21ade83d4f6f243c992
Steffen Vogel <post@steffenvogel.de>
2022-03-01 21:27:41 +0100

    api: add authentication

---
+++ cmd/server/auth.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"strings"
+)
+
+var (
+	authUsername = flag.String("api-username", "admin", "Username for API endpoint")
+	authPassword = flag.String("api-password", "", "Password for API endpoint")
+	authToken    = flag.String("api-token", "", "Bearer token for authentication")
+)
+
+func basicAuth(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		valid := false
+		if *authPassword != "" {
+			username, password, ok := r.BasicAuth()
+			valid = ok && username == *authUsername && password == *authPassword
+		} else if *authToken != "" {
+			authHeader := r.Header.Get("Authentication")
+			if authHeader != "" {
+				tokens := strings.Split(authHeader, " ")
+				switch tokens[0] {
+				case "Bearer":
+					valid = tokens[1] == *authToken
+				}
+			}
+		}
+
+		if valid {
+			next.ServeHTTP(w, r)
+		} else {
+			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		}
+	})
+}
